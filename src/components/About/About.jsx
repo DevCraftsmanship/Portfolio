a0200aaddff93f819819d37c5224169e0fba5a10
Tuhin Mohanty <tuhinmohanty0@gmail.com>
2025-05-10 00:27:19 +0530

    adding proper link for download option

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-  href="/Resume/Resume.docx"
+  href="https://github.com/DevCraftsmanship/Portfolio/raw/refs/heads/main/public/Resume/Resume.docx"
   download
   className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
   style={{
