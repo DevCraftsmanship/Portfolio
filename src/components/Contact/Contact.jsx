ac71d80903dcdb1770eb9c366e4552f345774f75
Tuhin Mohanty <tuhinmohanty0@gmail.com>
2025-05-11 12:35:17 +0530

    Adding contact info

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_axbtt7a",  // Replace with your EmailJS Service ID
-        "template_1ziboq3",  // Replace with your EmailJS Template ID
+        "service_qd310yw",  // Replace with your EmailJS Service ID
+        "template_iwftu9l",  // Replace with your EmailJS Template ID
         form.current,
-        "Rz7W9pVF0HdDryNNL"  // Replace with your EmailJS Public Key
+        "GWfhIDcBqVS07jUSN"  // Replace with your EmailJS Public Key
       )
       .then(
         () => {
